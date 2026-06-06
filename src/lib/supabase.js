6653c01f24aa7c7ef921cb7979047f04a1cf31f9
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-06 07:13:42 -0300

    feat!: replace cloudinary with supabase

---
+++ src/lib/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+import { SUPABASE_PUBLISHABLE_KEY, SUPABASE_URL } from '#root/config.js';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
