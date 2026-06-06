f3f5a239bc5f8fd71050914812f37e35dea8d7e4
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-06 10:06:47 -0300

    feat: use supabase upload operations

--- src/controllers/file.js
+++ src/controllers/file.js
@@ -1,15 +1,31 @@
+import { constants } from 'node:http2';
+
+import createHttpError from 'http-errors';
+
 import { prisma } from '#root/lib/prisma.js';
+import { supabase } from '#root/lib/supabase.js';
+
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR } = constants;
 
 export const getFile = async (req, res) => {
-	const file = await prisma.file.findFirst({ where: { id: req.params.fileId } });
+	const fileId = Number(req.params.fileId);
+	const file = await prisma.file.findFirst({ where: { id: fileId } });
 	res.render('file', { file });
 };
 
-export const download = async (req, res) => {
+export const download = async (req, res, next) => {
+	const fileId = Number(req.params.fileId);
+
 	const { name, path } = await prisma.file.findFirst({
 		select: { name: true, path: true },
-		where: { id: req.params.fileId },
+		where: { id: fileId },
 	});
 
-	res.download(path, name);
+	const { data, error } = await supabase.storage
+		.from('files')
+		.getPublicUrl(path, { download: true });
+
+	if (error != null) return next(createHttpError(HTTP_STATUS_INTERNAL_SERVER_ERROR, error.message));
+
+	res.redirect(`${data.publicUrl}${name}`); // Use name to get the original file name
 };
--- src/controllers/upload/upload.js
+++ src/controllers/upload/upload.js
@@ -1,5 +1,12 @@
+import { constants } from 'node:http2';
+
+import createHttpError from 'http-errors';
+
 import { prisma } from '#root/lib/prisma.js';
+import { supabase } from '#root/lib/supabase.js';
 import { renderForm } from '#root/utils/form-render.js';
+
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR } = constants;
 
 const inputs = [
 	{
@@ -11,10 +18,10 @@ const inputs = [
 	},
 ];
 
-const render = ({ params: { folderId = '' } }, res, errs) =>
+const render = ({ params: { folderId } }, res, errs) =>
 	renderForm(res, {
 		title: 'Upload file',
-		action: `/upload/${folderId}`,
+		action: `/upload/${folderId == null ? '' : `folder/${folderId}`}`,
 		errs,
 		hasUpload: true,
 		inputs,
@@ -23,14 +30,26 @@ const render = ({ params: { folderId = '' } }, res, errs) =>
 
 export const getUpload = (req, res) => render(req, res);
 
-export const upload = async (req, res) => {
+export const uploadFile = async (req, res, next) => {
 	const {
-		params: { folderId },
-		file: { originalname, size, path },
+		file: { buffer, mimetype, originalname, size },
 		user: { id },
 	} = req;
 
-	await prisma.upload.create({ data: { name: originalname, size, userId: id, folderId, path } });
+	const rawFolderId = req.params.folderId;
+	const folder = rawFolderId == null ? '' : `${rawFolderId}/`;
+	const date = new Date().toISOString();
+
+	const path = `${id}/${folder}${date}_${originalname}`;
+
+	const { error } = await supabase.storage
+		.from('files')
+		.upload(path, buffer, { contentType: mimetype });
+
+	if (error != null) return next(createHttpError(HTTP_STATUS_INTERNAL_SERVER_ERROR, error.message));
+
+	const folderId = Number(rawFolderId);
+	await prisma.file.create({ data: { name: originalname, size, userId: id, folderId, path } });
 
 	res.redirect('/');
 };
